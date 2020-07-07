6136670fa309ee28a49d9ce70b4fed418a6aaabd
WahyuS002 <syahputrawahyu61@gmail.com>
2020-07-07 14:58:08 +0700

    Slicing #1

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,4 +1,4 @@
-<!-- plugins:js -->
+{{-- <!-- plugins:js -->
 <script src="{{ asset('staradmin/assets/vendors/js/vendor.bundle.base.js') }}"></script>
 <script src="{{ asset('staradmin/assets/vendors/js/vendor.bundle.addons.js') }}"></script>
 <!-- endinject -->
@@ -10,4 +10,35 @@
 <!-- endinject -->
 <!-- Custom js for this page-->
 <script src="{{ asset('staradmin/assets/js/demo_1/dashboard.js') }}"></script>
-<!-- End custom js for this page-->
+<!-- End custom js for this page--> --}}
+
+<!-- build:js scripts/app.html.js -->
+<!-- jQuery -->
+<script src="{{ asset('flatkit/libs/jquery/jquery/dist/jquery.js') }}"></script>
+<!-- Bootstrap -->
+  <script src="{{ asset('flatkit/libs/jquery/tether/dist/js/tether.min.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/jquery/bootstrap/dist/js/bootstrap.js') }}"></script>
+<!-- core -->
+  <script src="{{ asset('flatkit/libs/jquery/underscore/underscore-min.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/jquery/jQuery-Storage-API/jquery.storageapi.min.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/jquery/PACE/pace.min.js') }}"></script>
+
+  <script src="{{ asset('flatkit/libs/scripts/config.lazyload.js') }}"></script>
+
+  <script src="{{ asset('flatkit/libs/scripts/palette.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-load.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-jp.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-include.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-device.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-form.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-nav.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-screenfull.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-scroll-to.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ui-toggle-class.js') }}"></script>
+
+  <script src="{{ asset('flatkit/libs/scripts/app.js') }}"></script>
+
+  <!-- ajax -->
+  <script src="{{ asset('flatkit/libs/jquery/jquery-pjax/jquery.pjax.js') }}"></script>
+  <script src="{{ asset('flatkit/libs/scripts/ajax.js') }}"></script>
+<!-- endbuild -->
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,4 +1,4 @@
-<!-- plugins:css -->
+{{-- <!-- plugins:css -->
 <link rel="stylesheet" href="{{ asset('staradmin/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css') }}">
 <link rel="stylesheet" href="{{ asset('staradmin/assets/vendors/iconfonts/ionicons/css/ionicons.css') }}">
 <link rel="stylesheet" href="{{ asset('staradmin/assets/vendors/iconfonts/typicons/src/font/typicons.css') }}">
@@ -16,5 +16,17 @@
 <!-- End Layout styles -->
 <link rel="shortcut icon" href="{{ asset('staradmin/assets/images/favicon.png') }}" />
 
-{{-- MY CSS --}}
-<link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+MY CSS --}}
+{{-- <link rel="stylesheet" href="{{ asset('assets/style.css') }}"> --}}
+
+<!-- style -->
+<link rel="stylesheet" href="{{ asset('flatkit/assets/animate.css/animate.min.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('flatkit/assets/glyphicons/glyphicons.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('flatkit/assets/font-awesome/css/font-awesome.min.css') }}" type="text/css" />
+<link rel="stylesheet" href="{{ asset('flatkit/assets/material-design-icons/material-design-icons.css') }}" type="text/css" />
+
+<link rel="stylesheet" href="{{ asset('flatkit/assets/bootstrap/dist/css/bootstrap.min.css') }}" type="text/css" />
+<!-- build:css ../assets/styles/app.min.css -->
+<link rel="stylesheet" href="{{ asset('flatkit/assets/styles/app.css') }}" type="text/css" />
+<!-- endbuild -->
+<link rel="stylesheet" href="{{ asset('flatkit/assets/styles/font.css') }}" type="text/css" />
