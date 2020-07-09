e5c51bf61dea1a41d07d3b123f803ea8fa59896d
WahyuS002 <syahputrawahyu61@gmail.com>
2020-07-09 09:13:40 +0700

    Final Slicing

--- resources/views/drivers/index.blade.php
+++ resources/views/drivers/index.blade.php
@@ -1,145 +1,146 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="content-wrapper">
-    <!-- Page Title Header Starts-->
-    <div class="row page-title-header">
-      <div class="col-12">
-        <div class="page-header">
-          <h4 class="page-title">Driver</h4>  
-          <div class="ml-auto">
-            <button type="button" class="btn btn-primary toolbar-item p-3" data-toggle="modal" data-target="#tambahModal">Tambah Driver</button>                    
-          </div>                
+
+<div ui-view class="app-body" id="view">
+  <div class="padding">
+    <div class="margin">
+      <div class="row d-flex align-items-center">
+        <div class="col-md-0">
+          <h5 class="mb-0 _300">Drivers</h5>
         </div>
-      </div>     
-    </div>     
+        <div class="col-md-0">
+          <a class="btn btn-sm btn-icon white" data-toggle="modal" data-target="#tambahModal">
+            <i class="fa fa-plus"></i>
+          </a>
+        </div>
+      </div>
+    </div>
     <div class="row">
-        @foreach ($drivers as $driver)
-        <div class="col-md-3 mb-4">
-            <div class="card rounded-m5">                                       
-                <div>
-                <a href="/driver/{{ $driver->id }}/edit" data-toggle="modal" data-target="#editModal{{ $driver->id }}">
-                        <img src="{{ url('storage/'.$driver->foto) }}" alt="tidak ada gambar" class="card-img-top p-3" style="height: 200px; width: 100%;">                            
+      <div class="col-md-9">
+        <div class="row">
+          @foreach ($drivers as $driver)
+            <div class="col-xs-6 col-sm-4 col-md-3">
+              <div class="box p-a-xs">
+                <a href="/driver/{{ $driver->id }}/edit" data-toggle="modal" data-target="#editModal{{ $driver->id }}"><img src="{{ url('storage/'.$driver->foto) }}" alt="" class="img-responsive fit-image"></a>
+                <div class="p-a-sm">
+                  <div class="text-ellipsis">{{ $driver->nama }}</div>
+                  <span class="text-muted">{{ $driver->nik }}</span>
+                  <div>
+                    <a href="/driver/{{ $driver->id }}/edit" data-toggle="modal" data-target="#editModal{{ $driver->id }}">
+                      {{-- <button class="btn btn-success btn-sm mr-3 rounded-m5">Edit</button> --}}
+                      <button class="btn btn-sm btn-fs rounded p-x-sm green">Edit</button>
                     </a>
-                </div>                    
-                <div class="card-footer">                        
-                    <div class="">
-                        <strong>{{ $driver->nama }}</strong>
-                    </div>
-                    <div class="text-muted">
-                        <span>{{ $driver->nik }}</span>
-                    </div>
-                    <div class="d-flex justify-content-center mt-4">
-                      <a href="/driver/{{ $driver->id }}/edit" data-toggle="modal" data-target="#editModal{{ $driver->id }}">
-                        <button class="btn btn-success btn-sm mr-3 rounded-m5 custom-button">Edit</button>  
-                      </a>                                                 
-                      <button type="button" class="btn btn-danger btn-sm rounded-m5 custom-button" data-toggle="modal" data-target="#deleteModal{{ $driver->id }}">
+                    <button type="button" class="btn btn-sm btn-fs rounded p-x-sm red" data-toggle="modal" data-target="#deleteModal{{ $driver->id }}">
                         Delete
-                    </button>                                         
-                    </div>                      
+                    </button>   
+                  </div>
                 </div>
-            </div>
+              </div>
+            </div>        
+          @endforeach
         </div>
-    @endforeach  
-    </div>            
+      </div>
+    </div>
   </div>
-  
-  <!-- Tambah Modal -->
-  <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+</div>
+
+<!-- Tambah Modal -->
+<div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content box-shadow-md black lt m-b">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Driver</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">            
+
+          <form action="/driver/store" method="POST" enctype="multipart/form-data">
+              @csrf               
+              <div class="form-group">
+                  <label for="nama">Nama</label>
+                  <input type="text" class="form-control" id="nama" name="nama">
+              </div>
+              <div class="form-group">
+                  <label for="nik">NIK</label>
+                  <input type="text" class="form-control" id="nik" name="nik">
+              </div>
+              <div class="form-group">
+                  <label for="foto">Foto</label>
+                  <input type="file" class="form-control" id="foto" name="foto">
+              </div>                
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah</button>
+              </div>
+          </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Edit Modal -->
+@foreach ($drivers as $driver)
+<div class="modal fade" id="editModal{{ $driver->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content box-shadow-md black lt m-b">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Driver</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">            
+          <form action="/driver/{{ $driver->id }}/update" method="POST" enctype="multipart/form-data">
+              @method('patch')
+              @csrf               
+              <div class="form-group">
+                  <label for="nama">Nama</label>
+                  <input type="text" class="form-control" id="nama" name="nama" value="{{ $driver->nama }}">
+              </div>
+              <div class="form-group">
+                  <label for="nik">NIK</label>
+                  <input type="text" class="form-control" id="nik" name="nik" value="{{ $driver->nik }}">
+              </div>
+              <div class="form-group">
+                  <label for="foto">Foto</label>
+                  <input type="file" class="form-control" id="foto" name="foto">
+              </div>                
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Edit</button>                                                      
+              </div>
+          </form>                                          
+      </div>
+    </div>
+  </div>
+</div>
+
+{{-- DELETE MODAL --}}
+
+<!-- Modal -->
+<form action="/driver/{{ $driver->id }}/delete" method="POST">
+  @csrf
+  @method('delete')   
+  <div class="modal fade" id="deleteModal{{ $driver->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
+      <div class="modal-content box-shadow-md black lt m-b">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Tambah Data Driver</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Yakin ingin menghapus driver?</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-        </div>
-        <div class="modal-body">            
-
-            <form action="/driver/store" method="POST" enctype="multipart/form-data">
-                @csrf               
-                <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" class="form-control" id="nama" name="nama">
-                </div>
-                <div class="form-group">
-                    <label for="nik">NIK</label>
-                    <input type="text" class="form-control" id="nik" name="nik">
-                </div>
-                <div class="form-group">
-                    <label for="foto">Foto</label>
-                    <input type="file" class="form-control" id="foto" name="foto">
-                </div>                
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="submit" class="btn btn-primary">Tambah</button>
-                </div>
-            </form>
+        </div>                                            
+        <div class="modal-footer d-flex justify-content-center">
+          <button type="button" class="btn btn-sm btn-success" data-dismiss="modal">Tidak</button>
+          <button type="submit" class="btn btn-sm btn-danger">Ya</button>
         </div>
       </div>
     </div>
   </div>
-
-  <!-- Edit Modal -->
-  @foreach ($drivers as $driver)
-  <div class="modal fade" id="editModal{{ $driver->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Tambah Data Driver</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">            
-            <form action="/driver/{{ $driver->id }}/update" method="POST" enctype="multipart/form-data">
-                @method('patch')
-                @csrf               
-                <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" class="form-control" id="nama" name="nama" value="{{ $driver->nama }}">
-                </div>
-                <div class="form-group">
-                    <label for="nik">NIK</label>
-                    <input type="text" class="form-control" id="nik" name="nik" value="{{ $driver->nik }}">
-                </div>
-                <div class="form-group">
-                    <label for="foto">Foto</label>
-                    <input type="file" class="form-control" id="foto" name="foto">
-                </div>                
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="submit" class="btn btn-primary">Edit</button>                                                      
-                </div>
-            </form>                                          
-        </div>
-      </div>
-    </div>
-  </div>
+</form>
+@endforeach
   
-  {{-- DELETE MODAL --}}
-  
-  <!-- Modal -->
-  <form action="/driver/{{ $driver->id }}/delete" method="POST">
-    @csrf
-    @method('delete')   
-    <div class="modal fade" id="deleteModal{{ $driver->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Yakin ingin menghapus driver?</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>                                            
-          <div class="modal-footer d-flex justify-content-center">
-            <button type="button" class="btn btn-sm btn-success" data-dismiss="modal">Tidak</button>
-            <button type="submit" class="btn btn-sm btn-danger">Ya</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </form>
-  @endforeach
-  
-  @endsection
+@endsection
--- resources/views/mobil/index.blade.php
+++ resources/views/mobil/index.blade.php
@@ -5,53 +5,53 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="content-wrapper">
-  <!-- Page Title Header Starts-->
-  <div class="row page-title-header">
-    <div class="col-12">
-      <div class="page-header">
-        <h4 class="page-title">Mobil</h4>  
-        <div class="ml-auto">
-          <button type="button" class="btn btn-primary toolbar-item p-3" data-toggle="modal" data-target="#tambahModal">Tambah Mobil</button>                    
-        </div>                
-      </div>
-    </div>      
-  </div>     
-  <div class="row">
-      @foreach ($mobils as $mobil)
-          <div class="col-md-3 mb-4">
-              <div class="card rounded-m5">                                       
-                  <div>
-                      <a href="/mobil/{{ $mobil->id }}/edit" data-toggle="modal" data-target="#editModal{{ $mobil->id }}">
-                          <img src="{{ url('storage/'.$mobil->foto) }}" alt="tidak ada gambar" class="card-img-top p-3"  style="height: 200px; width: 100%;">                            
-                      </a>
-                  </div>                    
-                  <div class="card-footer">                        
-                      <div class="">
-                          <strong>{{ $mobil->nama_mobil }}</strong>
-                      </div>
-                      <div class="">
-                          <span>{{ $mobil->brand->nama_brand }}</span>
-                      </div>
-                      <div class="d-flex justify-content-center mt-4">
-                          <a href="/mobil/{{ $mobil->id }}/edit" data-toggle="modal" data-target="#editModal{{ $mobil->id }}">
-                              <button class="btn btn-success btn-sm mr-3 rounded-m5 custom-button">Edit</button>  
-                          </a>
-                          <button type="button" class="btn btn-danger btn-sm rounded-m5 custom-button" data-toggle="modal" data-target="#deleteModal{{ $mobil->id }}">
-                              Delete
-                          </button>            
-                      </div>          
+
+<div ui-view class="app-body" id="view">
+  <div class="padding">
+    <div class="margin">
+      <div class="row d-flex align-items-center">
+        <div class="col-md-0">
+          <h5 class="mb-0 _300">Mobil</h5>
+        </div>
+        <div class="col-md-0">
+          <a class="btn btn-sm btn-icon white" data-toggle="modal" data-target="#tambahModal">
+            <i class="fa fa-plus"></i>
+          </a>
+        </div>
+      </div>         
+    </div>
+    <div class="row">
+      <div class="col-md-9">
+        <div class="row">
+          @foreach ($mobils as $mobil)
+            <div class="col-xs-6 col-sm-4 col-md-3">
+              <div class="box p-a-xs">
+                <a href="/mobil/{{ $mobil->id }}/edit" data-toggle="modal" data-target="#editModal{{ $mobil->id }}"><img src="{{ url('storage/'.$mobil->foto) }}" alt="" class="img-responsive fit-image"></a>
+                <div class="p-a-sm">
+                  <div class="text-ellipsis">{{ $mobil->nama_mobil }}</div>
+                  <span class="text-muted">{{ $mobil->brand->nama_brand }}</span>
+                  <div class="mt-2">
+                    <a href="/mobil/{{ $mobil->id }}/edit" data-toggle="modal" data-target="#editModal{{ $mobil->id }}">                      
+                      <button class="btn btn-sm btn-fs rounded p-x-sm green">Edit</button>
+                    </a>
+                    <button type="button" class="btn btn-sm btn-fs rounded p-x-sm red" data-toggle="modal" data-target="#deleteModal{{ $mobil->id }}">
+                        Delete
+                    </button>   
                   </div>
+                </div>
               </div>
-          </div>
-      @endforeach
-  </div>            
+            </div>        
+          @endforeach
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <!-- Tambah Modal -->
 <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content box-shadow-md black lt m-b">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Tambah Data Mobil</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -60,62 +60,56 @@
       </div>
       <div class="modal-body">            
 
-          <form action="/mobil/store" method="POST" enctype="multipart/form-data">
-              @csrf               
-              {{-- <div class="form-group">
-                  <label for="brand_id">Brand</label>
-                  <input type="text" class="form-control" id="brand_id" name="brand_id">
-              </div>--}}
+        <form action="/mobil/store" method="POST" enctype="multipart/form-data">
+            @csrf                             
+            <div class="form-group">
+              <label for="dropdown">Brand Mobil</label>        
+              <div>
+                <select class="w-100 dropdown-menu pos-stc inline dark mb-3" name="brand_id">
+                  <option value="">Select</option>
+                  @foreach($brands as $brand)
+                    <option value="{{ $brand->id }}">{{ $brand->nama_brand }}</option>
+                  @endforeach
+                </select>
+              </div>
+            </div>        
+            <div class="form-group">
+                <label for="no_plat">Nomor Plat</label>
+                <input type="text" class="form-control" id="no_plat" name="no_plat">
+                {{-- @error('no_plat')
+                  <div class="text-danger mt-2">{{ $message }}</div>
+                @enderror --}}
+            </div>
+            <div class="form-group">
+                <label for="nama_mobil">Mobil</label>
+                <input type="text" class="form-control" id="nama_mobil" name="nama_mobil">
+            </div>
+            <div class="form-group">
+                <label for="max_minyak">Kapasitas Minyak</label>
+                <input type="text" class="form-control" id="max_minyak" name="max_minyak">
+            </div>                
+            <div class="form-group">
+                <label for="foto">Foto</label>
+                <input type="file" class="form-control" id="foto" name="foto">
+            </div>                
+            <hr>            
+              <div class="d-flex bd-highlight mb-3">
+                <button type="submit" class="mr-auto p-2 bd-highlight float-right btn btn-sm btn-fw black">Tambah Brand</button>
+                <button type="button" class="btn btn-sm btn-secondary p-2 bd-highlight mr-2" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-sm btn-primary p-2 bd-highlight">Tambah</button>
+              </div>            
+        </form>
 
-              <div class="form-group">
-                <label for="dropdown">Brand Mobil</label>                 
-        
-                <div>
-                  <select class="form-control" name="brand_id">
-                    <option value="">Select</option>
-                    @foreach($brands as $brand)
-                      <option value="{{ $brand->id }}">{{ $brand->nama_brand }}</option>
-                    @endforeach
-                  </select>
-                </div>
-              </div>
-          
-
-              <div class="form-group">
-                  <label for="no_plat">Nomor Plat</label>
-                  <input type="text" class="form-control" id="no_plat" name="no_plat">
-                  {{-- @error('no_plat')
-                    <div class="text-danger mt-2">{{ $message }}</div>
-                  @enderror --}}
-              </div>
-              <div class="form-group">
-                  <label for="nama_mobil">Mobil</label>
-                  <input type="text" class="form-control" id="nama_mobil" name="nama_mobil">
-              </div>
-              <div class="form-group">
-                  <label for="max_minyak">Kapasitas Minyak</label>
-                  <input type="text" class="form-control" id="max_minyak" name="max_minyak">
-              </div>                
-              <div class="form-group">
-                  <label for="foto">Foto</label>
-                  <input type="file" class="form-control" id="foto" name="foto">
-              </div>                
-              <div class="modal-footer">
-                <button type="submit" class="float-right btn btn-warning">Tambah Brand</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Tambah</button>
-              </div>
-          </form>
       </div>
     </div>
   </div>
 </div>
 
-<!-- Edit Modal -->
+<!-- EDIT MODAL -->
 @foreach ($mobils as $mobil)
 <div class="modal fade" id="editModal{{ $mobil->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content box-shadow-md black lt m-b">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Edit Data Mobil</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -127,10 +121,9 @@
               @method('patch')
               @csrf               
               <div class="form-group">
-                <label for="dropdown">Brand Mobil</label>
-        
+                <label for="dropdown">Brand Mobil</label>        
                 <div>
-                  <select class="form-control" name="brand_id">
+                  <select class="w-100 dropdown-menu pos-stc inline dark mb-3" name="brand_id">
                     <option value="">Select</option>
                     @foreach($brands as $brand)
                       <option value="{{ $brand->id }}">{{ $brand->nama_brand }}</option>
@@ -163,16 +156,15 @@
     </div>
   </div>
 </div>
+<!-- /EDIT MODAL -->
 
 {{-- DELETE MODAL --}}
-
-<!-- Modal -->
 <form action="/mobil/{{ $mobil->id }}/delete" method="POST">
   @csrf
   @method('delete')   
   <div class="modal fade" id="deleteModal{{ $mobil->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
+      <div class="modal-content box-shadow-md black lt m-b">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Yakin ingin menghapus mobil?</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -187,6 +179,7 @@
     </div>
   </div>
 </form>
-  @endforeach
+@endforeach
+{{-- /DELETE MODAL --}}
 
 @stop
