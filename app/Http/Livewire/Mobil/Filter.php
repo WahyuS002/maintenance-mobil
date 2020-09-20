22a48b4edce31780726295f4f54a562f4c7a979a
WahyuS002 <syahputrawahyu61@gmail.com>
2020-09-20 14:47:42 +0700

    added livewire

---
+++ app/Http/Livewire/Mobil/Filter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Mobil;
+
+use Livewire\Component;
+
+class Filter extends Component
+{
+    public function render()
+    {
+        return view('livewire.mobil.filter');
+    }
+}
